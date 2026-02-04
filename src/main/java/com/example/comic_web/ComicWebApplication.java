f1601ac38852da22fe3fb510fab264528c7f0133
Tokoroa <HaiPV.B23CN281@stu.ptit.edu.vn>
2026-02-04 23:06:28 +0700

    First commit

---
+++ src/main/java/com/example/comic_web/ComicWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.comic_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComicWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComicWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/comic_web/ComicWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.comic_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComicWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
